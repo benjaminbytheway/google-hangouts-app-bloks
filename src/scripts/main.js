bb28b28de815c6a4e35d0e8ac0b8b1994906488a
Benjamin Bytheway <bbythewa@adobe.com>
2016-04-28 17:27:53 -0700

    I don't think anything changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,26 @@
 'use strict';
 
 require([
-		'game',
-		'player'
-	], function (game, Player) {
+    'game',
+    'player'
+  ], function (game, Player) {
 
-		var 
-			initializedPromise = new Promise(function (resolve, reject) {
-				gapi.hangout.onApiReady.add(function(evt) {
-		      if (evt.isApiReady) {
-		        resolve();
-		      }
-		    });
-			});
+    var 
+      initializedPromise = new Promise(function (resolve, reject) {
+        gapi.hangout.onApiReady.add(function(evt) {
+          if (evt.isApiReady) {
+            resolve();
+          }
+        });
+      });
 
-		//------------------------------------------------------------------------
+    //------------------------------------------------------------------------
     // Initialize listeners
     //------------------------------------------------------------------------
     initializedPromise
-    	.then(function () {
-    		// TODO:
-    	});
+      .then(function () {
+        // TODO:
+      });
 
-	});
-	
+  });
+  

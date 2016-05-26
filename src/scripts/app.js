234310eefdee43397acf0024578ff11b99aa3fa6
Benjamin Bytheway <bbythewa@adobe.com>
2016-05-26 13:52:46 -0700

    Refactored to use angular completely

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,31 +1,272 @@
 'use strict';
 
 define('app', [
-    'game',
-    'player',
-    'events',
-    'ui'
-  ], function (game, player, events, ui) {
+    // no dependencies
+  ], function () {
 
-    var 
-      app;
+    var
+      app,
+      parseState = function (state) {
+        var 
+          property,
+          stateParsed = {};
+
+        for (property in state) {
+          if (!state.hasOwnProperty(property)) {
+            continue;
+          }
+
+          try {
+            stateParsed[property] = JSON.parse(state[property]);
+          } catch (e) {}
+        }
+
+        return stateParsed;
+      },
+      serializeState = function (state) {
+        var 
+          property,
+          stateSerialized = {};
+
+        for (property in state) {
+          if (!state.hasOwnProperty(property)) {
+            continue;
+          }
+
+          try {
+            stateSerialized[property] = JSON.stringify(state[property]);
+          } catch (e) {}
+        }
+
+        return stateSerialized;
+      },
+      STAGES = {
+        LOBBY: 1, // waiting for game to start
+        LOADING: 2,
+        PLAY: 3,
+        DETERMINE_WINNER: 4,
+        CONGRATULATIONS: 5
+      };
 
     app = angular.module('app', [
         // TODO: dependencies
       ]);
 
     // run blocks
-    app.run(['$rootScope', function ($rootScope) {
-        $rootScope.game = game;
+    app
 
-        // hook up the UI
-        $rootScope.join = function () {
-          events.trigger('ui.1.button.join.click');
-        };
+      // initialize
+      .run(['$rootScope', function ($rootScope) {
 
-        events.on('ui.1.game.players.changed', function (data) {
+        // Get my participant
+        $rootScope.me = gapi.hangout.getLocalParticipant();
+
+        // get initial state, or initialize
+        $rootScope.state = parseState(gapi.hangout.data.getState()) || {};
+
+        //--------------------------------------------------------------------
+        // Initialize the game (if not already initialized)
+        //--------------------------------------------------------------------
+
+        // game
+        if (!$rootScope.state.game) {
+          $rootScope.state.game = {};
+        }
+
+        // game.currentStage
+        if (!$rootScope.state.game.currentStage) {
+          $rootScope.state.game.currentStage = STAGES.LOBBY;
+        }
+
+        // players
+        if (!$rootScope.state.players) {
+          $rootScope.state.players = [];
+        }
+        
+        //--------------------------------------------------------------------
+        // get notifications from others in the hangout
+        //--------------------------------------------------------------------
+        gapi.hangout.data.onStateChanged.add(function(event) {
+          // event.addedKeys
+          // event.metadata
+          // event.removedKeys
+          // event.state
+
+          var
+            state,
+            key,
+            value,
+            metadata;
+          
+          state = parseState(event.state);
+
+          // If we sent the message, then return
+          // if (state.writerId === $rootScope.me.id) {
+          //   return;
+          // } else {
+          //   delete state.writerId;
+          // }
+
+          // TODO: Probably will need this in the future
+          for (key in state) {
+            value = state[key];
+            metadata = event.metadata[key];
+
+            if (key === 'game') {
+              console.log('game updated elsewhere');
+              console.log(value);
+            }
+          }
+
+          $rootScope.state = state;
+
           $rootScope.$apply();
         });
+
+        //--------------------------------------------------------------------
+        // notify others
+        //--------------------------------------------------------------------
+        $rootScope.$watch('state', function (newValue, oldValue, scope) {
+          // console.log('---------------------$watch-------------------');
+          // console.log('NEW');
+          // console.log(newValue);
+          // console.log('OLD');
+          // console.log(oldValue);
+
+          // set who is writing
+          // $rootScope.state.writerId = $rootScope.me.id;
+
+          // submit the changes
+          gapi.hangout.data.submitDelta(serializeState($rootScope.state));
+
+        }, true); // TODO: Figure out a way not to do object equality
+
+
+        //--------------------------------------------------------------------
+        // Players
+        //--------------------------------------------------------------------
+        // TODO: Move this into it's own peice of code
+
+        $rootScope.computers = 0;
+
+        $rootScope.addPlayer = function (config) {
+          var
+            player,
+            players;
+
+          players = $rootScope.state.players;
+          
+          if (
+            // make sure we have a player
+            config.player &&
+            // make sure we have an id for the player
+            config.player.id &&
+            // make sure that we only add up to 4 players
+            players.length < 4 &&
+            // make sure the player is added only once
+            !$rootScope.findPlayerById(config.player.id)
+          ) {
+            player = {};
+
+            // type
+            if (config.player.type) {
+              player.type = config.player.type;
+            } else {
+              throw new Error('config.player.type was not provided');
+            }
+            
+            // human player
+            if (player.type === 'human') {
+              
+              // id
+              if (config.player.id) {
+                player.id = config.player.id;
+              } else {
+                throw new Error('config.player.id was not provided');
+              }
+
+              // name
+              if (config.player.name) {
+                player.name = config.player.name;
+              } else {
+                throw new Error('config.player.name was not provided');
+              }
+
+              // image
+              if (config.player.image) {
+                player.image = config.player.image;
+              } else {
+                // TODO: Default image?
+              }
+
+            // computer player
+            } else if (player.type === 'computer') {
+
+              // id
+              player.id = 'computer-' + Math.floor(Math.random() * 10000);
+
+              // name
+              player.name = 'Computer ' + $rootScope.computers + 1;
+
+              // image
+              // TODO: find a computer image
+
+            } else {
+              throw new Error('config.player.type is not a supported type (either "human", or "computer")');
+            }
+
+            // add the player
+            players.push(player);
+
+            // if we haven't set a host yet, set it
+            if (
+              !$rootScope.state.host &&
+              player.type === 'human'
+            ) {
+              $rootScope.state.host = player;
+            }
+
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        $rootScope.findPlayerById = function (id) {
+          var
+            i, l,
+            player,
+            players;
+
+          players = $rootScope.state.players;
+
+          for (i = 0, l = players.length; i < l; i++) {
+            player = players[i];
+
+            if (player.id === id) {
+              return player;
+            }
+          }
+
+          return null;
+        };
+
+
+        //--------------------------------------------------------------------
+        // hook up the UI
+        //--------------------------------------------------------------------
+        $rootScope.join = function () {
+  
+          $rootScope.addPlayer({
+            player: {
+              type: 'human',
+              id: $rootScope.me.person.id,
+              name: $rootScope.me.person.displayName,
+              image: $rootScope.me.person.image.url
+            }
+          });
+
+        };
 
       }]);
     
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,48 +16,7 @@ require([
             resolve();
           }
         });
-      }),
-      parseState = function (state) {
-        var 
-          property,
-          stateParsed = {};
-
-        for (property in state) {
-          if (!state.hasOwnProperty(property)) {
-            continue;
-          }
-
-          try {
-            stateParsed[property] = JSON.parse(state[property]);
-          } catch (e) {}
-        }
-
-        return stateParsed;
-      },
-      serializeState = function (state) {
-        var 
-          property,
-          stateSerialized = {};
-
-        for (property in state) {
-          if (!state.hasOwnProperty(property)) {
-            continue;
-          }
-
-          try {
-            stateSerialized[property] = JSON.stringify(state[property]);
-          } catch (e) {}
-        }
-
-        return stateSerialized;
-      },
-      state,
-      me,
-      updateState = function () {
-        state.writerId = me.id;
-        gapi.hangout.data.submitDelta(serializeState(state));
-      };
-
+      });
     
     hangoutOnApiReadyPromise
       //----------------------------------------------------------------------
@@ -68,44 +27,7 @@ require([
         // start the app
         angular.bootstrap(document, ['app']);
 
-        // Get my participant
-        me = gapi.hangout.getLocalParticipant();
-
-        // Get the current state of the app and determine where to bring this enabled user into the flow
-        state = parseState(gapi.hangout.data.getState());
-
-        // Start the game
-        // TODO: figure out conflicts when two people join at the same time
-        if (!state.game) {
-
-          // update State with the new game config
-          state.game = {
-            currentStage: game.STAGES.LOBBY
-          };
-          updateState();
-
-          // start the game
-          game.initialize({
-            game: state.game
-          });
-
-          // don't need this anymore with angular
-          // events.trigger('ui.1.game.players.changed', {
-          //   game: game
-          // });
-
-        } else {
-
-          game.initialize({
-            game: state.game
-          });
-
-          // don't need this anymore with angular
-          // events.trigger('ui.1.game.players.changed', {
-          //   game: game
-          // });
-
-        }
+        // TODO: loading screen fade out, app fade in...
 
       })
       //----------------------------------------------------------------------
@@ -123,41 +45,6 @@ require([
           // event.metadata
           // event.removedKeys
           // event.state
-
-          state = parseState(event.state);
-
-          // If we sent the message, then return
-          if (state.writerId === me.id) {
-            return;
-          }
-
-          // handle events
-          if (state.eventName === 'game.addPlayer') {
-            game.updatePlayers(state.game.players);
-
-            events.trigger('ui.1.game.players.changed', {
-              game: game
-            });
-          }
-
-          if (state.eventName === 'game.removePlayer') {
-            game.updatePlayers(state.game.players);
-
-            events.trigger('ui.1.game.players.changed', {
-              game: game
-            });
-          }
-
-          if (state.eventName === 'game.setHost') {
-            game.setHost({
-              player: state.game.host,
-              supressEvent: true
-            });
-
-            events.trigger('ui.1.game.players.changed', {
-              game: game
-            });
-          }
         });
 
         // listener for enabled participants
@@ -187,79 +74,6 @@ require([
             }
 
           }
-        });
-
-        //--------------------------------------------------------------------
-        // from me
-        //--------------------------------------------------------------------
-        events.on('game.addPlayer', function (data) {
-
-          // tell UI to update
-          // don't need this anymore with angular
-          // events.trigger('ui.1.game.players.changed', {
-          //   game: game
-          // });
-
-          // tell everyone else to update
-          state.game.players = game.players;
-          state.eventName = 'game.addPlayer';
-          updateState();
-        });
-
-        events.on('game.removePlayer', function (data) {
-          
-          // tell UI to update
-          // don't need this anymore with angular
-          // events.trigger('ui.1.game.players.changed', {
-          //   game: game
-          // });
-
-          // tell everyone else to update
-          state.game.players = game.players;
-          state.eventName = 'game.removePlayer';
-          updateState();
-        });
-
-        events.on('game.setHost', function (data) {
-
-          // tell the layout to display a notice
-          if (me.id === game.host.id) {
-            gapi.hangout.layout.displayNotice('You have just been made the game host.');
-          }
-
-          // tell UI to update
-          // don't need this anymore with angular
-          // events.trigger('ui.1.game.players.changed', {
-          //   game: game
-          // });
-
-          // tell everyone else to update
-          state.game.host = game.host;
-          state.eventName = 'game.setHost';
-          updateState();
-        });
-
-        events.on('game.startStage', function (data) {
-          state.game.stage = game.stage;
-          state.eventName = 'game.startStage';
-
-          updateState();
-        });
-
-        //--------------------------------------------------------------------
-        // from ui
-        //--------------------------------------------------------------------
-        events.on('ui.1.button.join.click', function () {
-
-          game.addPlayer({
-            player: {
-              type: 'human',
-              id: me.id,
-              name: me.person.displayName,
-              image: me.person.image.url
-            }
-          });
-
         });
 
       });
